22801f8f187a86eabe10fe2eb34aca97a20235b9
Joseph Utecht <joseph.utecht@arkansas.gov>
2014-04-30 10:32:33 -0500

    modified angular

--- app.js
+++ app.js
@@ -11,16 +11,30 @@ function ListCtrl($scope){
     $scope.osActive = function(input){
         return $scope.os.indexOf(input) > -1;
     };
+    $scope.hasOS = function(game, test_os){
+        for(var b in game.binaries){
+            if(game.binaries[b] == test_os){
+                return true;
+            }
+        }
+        return false;
+    };
+    $scope.osURL = function(game, test_os){
+        for(var b in game.binaries){
+            if(game.binaries[b] == test_os){
+                return game.binary_url[b];
+            }
+        }
+        return "";
+    };
     $scope.osFilter =  function(input){
         if($scope.os.length > 0){
             for(var o in $scope.os){
-                for(var b in input.binaries){
-                    if(input.binaries[b] == $scope.os[o]){
-                        return true;
-                    }
+                if(typeof input.binaries == 'undefined' || input.binaries.indexOf($scope.os[o]) == -1){
+                    return false;
                 }
             }
-            return false;
+            return true;
         } else {
             return true;
         }
